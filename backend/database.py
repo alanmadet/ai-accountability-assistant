5bb61ea922db767aaa55efec7095381a4f3a3b0d
Alan Madet <alanmadet@Alans-MacBook-Pro.local>
2026-05-18 18:15:26 -0400

    Migrated to using Postgres in AWS

--- backend/database.py
+++ backend/database.py
@@ -1,12 +1,19 @@
+import os
+
+from dotenv import load_dotenv
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "sqlite:///./tasks.db"
+load_dotenv()
+
+DATABASE_URL = os.getenv(
+    "DATABASE_URL"
+)
 
 engine = create_engine(
-    DATABASE_URL,
-    connect_args={"check_same_thread": False}
+    DATABASE_URL
 )
 
 SessionLocal = sessionmaker(
